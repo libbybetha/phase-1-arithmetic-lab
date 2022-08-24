e983456f8259f588c249bfb8eef62ad99d709981
Beth Njuguna <libbybetha@gmail.com>
2022-08-24 14:34:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 *num2;
 
-// Write your code here
+let random = Math.floor(Math.random()*10)+1;
+let num3=20;
+let num4=8;
+let mod =num3%num4;
+let max =Math.max(7,9,12,15,17,20);
+
+function mod(num3,num4){
+    num3=20;
+    num4=8;
+    mod = num3%num4;
+}
+mod()
+function max(){
+    Math.max(7, 9,12,15,17,20);
+    max;
+}
